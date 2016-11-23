80698d7518c6cb4fd2d9fc47316fe8bef60453cc
xortiz <cuchumino@gmail.com>
2016-11-23 13:10:55 -0500

    added javascript lines to automatically run animation on scroll

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,8 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
     
-    var points = document.getElementsByClassName('point');
+    
     
     for (i = 0; i < points.length; i += 1){
         var revealPoint = function() {
@@ -10,5 +12,24 @@ var animatePoints = function() {
         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
         };
         revealPoint();        
-    }
+    }     
 };
+
+window.onload = function() {
+    
+    if (window.innerHeight > 950) {
+         animatePoints(pointsArray);
+     }
+    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
+    window.addEventListener('scroll', function(event) {
+
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance)
+        {
+            animatePoints(pointsArray);
+        }
+    });
+}
